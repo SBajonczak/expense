128d10bc90ceeeb757cc8cec1627a23f2bcf1902
Sascha Peter Bajonczak <Sascha.Bajonczak@realcore.de>
2020-01-03 06:58:37 +0100

    added webfrontend and refactored cqrs

---
+++ Backend/Controllers/InvoiceCommandController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SBA.Expense.Commands.Services;
+using SBA.Expense.Models;
+using SBA.Expense.Queries.Sevices;
+
+namespace SBA.Expense.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InvoiceCommandController : ControllerBase
+    {
+
+        IInvoiceQueryService queryService;
+        IInvoiceCommandService commandService;
+        private readonly ILogger<InvoiceController> _logger;
+
+        public InvoiceCommandController(ILogger<InvoiceController> logger, IInvoiceQueryService queryService, IInvoiceCommandService commandService)
+        {
+            this.commandService = commandService;
+            this.queryService= queryService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task AddInvoiceEntry([FromBody]Invoice invoice ){
+            await commandService.SaveInvoice(invoice.ID,invoice.UserId, invoice.Date);
+        }
+
+        [HttpGet]
+        public async Task<List<Invoice>> GetInvoicesAsync([FromQuery] string username){
+            var r = await queryService.GetInvoices(username);
+            return r;
+        }
+    }
+}
---
+++ Backend/Controllers/InvoiceController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SBA.Expense.Commands.Services;
+using SBA.Expense.Models;
+using SBA.Expense.Queries.Sevices;
+
+namespace SBA.Expense.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InvoiceController : Controller
+    {
+
+        IInvoiceQueryService queryService;
+
+        private readonly ILogger<InvoiceController> _logger;
+
+        public InvoiceController(ILogger<InvoiceController> logger, IInvoiceQueryService queryService )
+        {
+            this.queryService = queryService;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<List<Invoice>> Get([FromQuery] string username)
+        {
+            return await queryService.GetInvoices(username);
+        }
+    }
+}
